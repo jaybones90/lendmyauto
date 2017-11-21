ae1873769ed7e355f1a071433537034614a15577
Jason Ainsworth <jasoncruze@gmail.com>
2017-11-20 18:30:58 -0800

    add in drop downs for both start and end date in the view, add in private methods for start and end date params in the controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,9 +2,34 @@ class VehiclesController < ApplicationController
 
   def new
     @user = User.find(params[:user_id])
-    @vehicle = Vehicle.new()
+    @vehicle = @user.vehicles.new()
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @vehicle = @user.vehicles.new(vehicle_params)
+    @vehicle.availability_start = availability_start_params
+    @vehicle.availability_end = availability_end_params
+    binding.pry
+    if @vehicle.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:make, :model, :year, :milage, :transmission, :color, :seats, :category)
+  end
+
+  def availability_start_params
+    Date.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)
+  end
+
+  def availability_end_params
+    Date.civil(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
+  end
+
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,4 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
-  validates :make, :model, :year, :milage, :transmission, :color, :seats, :category, :all_wheel_drive,
-            :pets_allowed, :smoking_allowed, :daily_price, :availability_start, :availability_end, :presence => true
+  validates :make, :model, :year, :milage, :transmission, :color, :seats, :category, :availability_start, :presence => true
 end
