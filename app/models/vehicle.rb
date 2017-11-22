933481ec5def3d1cf2dd76ec1c31392685247b86
Jason Ainsworth <jasoncruze@gmail.com>
2017-11-21 17:32:04 -0800

    add in scope for last 3 created vehicles, add in feature display on pages home view for popular cars scope

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,12 @@
 class Vehicle < ApplicationRecord
   has_many :images
   has_many :features
+
   accepts_nested_attributes_for :features, :images
+
   belongs_to :user
+
   validates :make, :model, :year, :milage, :transmission, :color, :seats, :category, :availability_start, :presence => true
+
+  scope :popular_cars, -> {limit(3)}
 end
