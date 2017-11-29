02b9f41a91ce5afa966815bbc602ea5bf8a4a63e
Jason Ainsworth <jasoncruze@gmail.com>
2017-11-28 17:46:34 -0800

    create seed file with fake data for users, vehicles, and account addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,29 +30,38 @@ class Seed
         city: Faker::Address.city,
         state: Faker::Address.state,
         zip_code: Faker::Address.zip_code,
-        account_id: i + 1
+        account_id: (i + 1)
       )
       puts "created #{Address.all.count} account addresses"
     end
   end
 
   def create_vehicles
-    doors = [2,4]
     transmissions = ["Automatic", "Manual"]
+    random = Random.new()
+    doors = [2,4]
+    seats = [2,4,5,6,7,8]
+    makes = ["Audi", "BMW", "Chevy", "Toyota", "Subaru"]
+    models = ["Accord", "Dakota", "Forester", "A4", "Suburban"]
+    categories = ["Suv","Truck", "Sedan", "Coupe", "Van", "Wagon", "Convertible","Sports Car", "Hybrid/Electric",  "Luxury"]
     5.times do |i|
       Vehicle.create!(
-        make: Faker::Vehicle.make,
-        model: Faker::Vehicle.model,
-        year: Faker::Vehicle.year,
-        milage: Faker::Vehicle.mileage,
-        color: Faker::Vehicle.color,
-        transmission: transmissions.sample
-        doors:
-        category:
-        daily_price:
-        account_id: i + 1
+        make: makes.sample,
+        model: models.sample,
+        year: random.rand(1998..2018),
+        milage: random.rand(0..120000),
+        color: Faker::Color.color_name,
+        transmission: transmissions.sample,
+        seats: seats.sample,
+        doors: doors.sample,
+        category: categories.sample,
+        daily_price: Faker::Commerce.price,
+        account_id: (i + 1),
+        availability_start: Date.civil(2017, 12, random.rand(30)),
+        availability_end: Date.civil(2018, random.rand(12), random.rand(30))
       )
     end
+    puts "created #{Vehicle.all.count} vehicles"
   end
 
 
@@ -62,3 +71,4 @@ seed = Seed.new
 seed.create_features
 seed.create_users
 seed.create_account_addresses
+seed.create_vehicles
