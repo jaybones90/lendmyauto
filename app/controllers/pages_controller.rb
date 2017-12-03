203ea53b80b338ea434859d38867decfe1ef2045
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-03 14:06:23 -0800

    add in search bar partial to pages home view page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @vehicles = Vehicle.all.limit(3)
     @location = Location.new()
+    @reservation = Reservation.new()
   end
-  
+
 end
