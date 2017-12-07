7185c1c7e3f3e4c0f01dfc48e5b7f0d0a9120552
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-07 13:59:42 -0800

    add in passing spec test for associations in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
 
   belongs_to :vehicle, inverse_of: :reviews
 
-  belongs_to :reviewer_account, inverse_of: :reviews
+  belongs_to :reviewer_account, class_name: "Account", inverse_of: :reviews
 
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to :vehicle }
+  it { should belong_to :reviewer_account }
 end
