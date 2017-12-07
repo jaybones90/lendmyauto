0e382e48e5da9410e33da5daaee4378537b54e08
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-07 14:02:01 -0800

    add in passing spec test for associations, email presence,  and save_account method in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe User do
+  let(:user){ FactoryBot.create(:user) }
   it { should validate_presence_of :email }
   it { should have_one :account }
+
+  it "creates an account for a new user" do
+    expect(user.account.user_id).to eq(user.id)
+  end
 end
