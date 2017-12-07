654e4383a72c6a6239cb011a8e0b94ba401ef6dc
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-07 12:39:08 -0800

    add in passing spec tests for assocations and presence validations on location model

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe Location do
   it { should have_many :vehicles }
+  it { should have_many :reservations }
+  it { should validate_presence_of :street_address}
+  it { should validate_presence_of :city}
+  it { should validate_presence_of :state}
+  it { should validate_presence_of :zip_code}
+  it { should validate_presence_of :country}
 end
