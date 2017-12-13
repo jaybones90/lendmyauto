197516511680fb5ffb1aa27d9920c1d93611beaa
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-12 22:05:23 -0800

    add numericiality and length validations to user_phone_number and length validation on user first and last names

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,11 +7,13 @@ class Account < ApplicationRecord
   has_one :drivers_license, dependent: :destroy
   has_one :image, as: :imageable, dependent: :destroy
 
-  has_many :reservations, :foreign_key => "renter_account_id", inverse_of: :renter_account, class_name: 'Reservation', dependent: :nullify
-  has_many :loans, :foreign_key => "lender_account_id", inverse_of: :lender_account, class_name: 'Reservation', dependent: :nullify
-  has_many :owned_vehicles, :foreign_key => "owner_account_id", class_name: "Vehicle", inverse_of: :owner_account, dependent: :destroy
+  has_many :reservations, :foreign_key => "renter_account_id", inverse_of: :renter_account,
+            class_name: 'Reservation', dependent: :nullify
+  has_many :loans, :foreign_key => "lender_account_id", inverse_of: :lender_account,
+            class_name: 'Reservation', dependent: :nullify
+  has_many :owned_vehicles, :foreign_key => "owner_account_id", class_name: "Vehicle",
+            inverse_of: :owner_account, dependent: :destroy
   has_many :reviews, inverse_of: :reviewer_account, dependent: :destroy
-
 
   accepts_nested_attributes_for :image
 
@@ -21,16 +23,20 @@ class Account < ApplicationRecord
   validates_associated :loans
   validates_associated :owned_vehicles
 
+  validate :age_over_18, on: :update
 
-  validate :validate_age_over_18, on: :update
+  validates :user_birth_date, presence: true,
+            on: :update, unless: :skip_birthdate_validation
 
-  validates :user_birth_date, presence: true, on: :update, unless: :skip_birthdate_validation
-  validates :user_phone_number, presence: true, on: :update, unless: :skip_phone_validation
-  validates :user_first_name, :user_last_name, presence: true, on: :update
+  validates :user_phone_number, presence: true, :numericality => { only_integer: true },
+            :length => { is: 10 }, on: :update, unless: :skip_phone_validation
+
+  validates :user_first_name, :user_last_name, presence: true,
+            :length => { in: 2..20 }, on: :update
 
   private
 
-  def validate_age_over_18
+  def age_over_18
     if user_birth_date.present? && user_birth_date > 18.years.ago
        errors.add(:user_birth_date, 'You Must Be Over 18 Years Old To Use This Service')
     end
