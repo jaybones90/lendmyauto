71a13576010dc5525df44676a314d86620f3d630
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-13 16:54:39 -0800

    add in validations and passing tests for number, expiration_date, and issue_date in drivers license model

--- app/models/drivers_license.rb
+++ app/models/drivers_license.rb
@@ -1,10 +1,12 @@
 class DriversLicense < ApplicationRecord
 
   belongs_to :account, inverse_of: :drivers_license
-
   has_one :image, as: :imageable, dependent: :destroy
-  validates_associated :image
-
   accepts_nested_attributes_for :image
+
+  validates_associated :image
+  validates :number, presence: true, :length => { maximum: 15 }, uniqueness: true
+  validates :expiration_date, presence: true
+  validates :issue_date, presence: true
 
 end
--- spec/models/drivers_license_spec.rb
+++ spec/models/drivers_license_spec.rb
@@ -4,5 +4,9 @@ describe DriversLicense do
   it { should belong_to :account }
   it { should have_one :image }
   it { should accept_nested_attributes_for :image}
-
+  it { should validate_presence_of :number }
+  it { should validate_presence_of :expiration_date }
+  it { should validate_presence_of :issue_date }
+  it { should validate_length_of(:number).is_at_most(15) }
+  it { should validate_uniqueness_of :number }
 end
