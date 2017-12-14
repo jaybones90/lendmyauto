739f07c0791dc72175ad67e6d6bbdaea63b0db0e
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-13 16:57:44 -0800

    add in validations and passing tests for description in feature model

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,4 +1,6 @@
 class Feature < ApplicationRecord
   has_and_belongs_to_many :vehicles
 
+  validates :description, presence: true, uniqueness: true
+
 end
--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Feature do
   it { should have_and_belong_to_many :vehicles }
+  it { should validate_presence_of :description }
+  it { should validate_uniqueness_of :description }
 end
