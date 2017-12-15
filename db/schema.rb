715227df6e1468ea36670cdd35d9fcdecc1c9986
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-14 19:41:16 -0800

    add is_admin? column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206014532) do
+ActiveRecord::Schema.define(version: 20171215033934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 20171206014532) do
     t.integer "account_id"
     t.string "provider"
     t.string "uid"
+    t.boolean "is_admin?"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
