fc431f814a2e303557b8cc454ef587641435b868
Jason Ainsworth <jasoncruze@gmail.com>
2017-12-14 19:55:33 -0800

    add in admin column to users table, confirm that admin link is working if user.admin is true

--- db/migrate/20171215033934_add_is_admin_column_to_users.rb
+++ db/migrate/20171215033934_add_is_admin_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddIsAdminColumnToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :is_admin?, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -131,7 +131,7 @@ ActiveRecord::Schema.define(version: 20171215033934) do
     t.integer "account_id"
     t.string "provider"
     t.string "uid"
-    t.boolean "is_admin?"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
